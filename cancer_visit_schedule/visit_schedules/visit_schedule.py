186c65317b8dac0f99ea2b696cdd0547b490455b
Ame Diphoko <diphokoame@gmail.com>
2025-02-14 11:30:44 +0200

    add: new schedule for 6 months intervals

--- cancer_visit_schedule/visit_schedules/visit_schedule.py
+++ cancer_visit_schedule/visit_schedules/visit_schedule.py
@@ -1,6 +1,7 @@
 from edc_visit_schedule import VisitSchedule, site_visit_schedules
 
 from .schedule import schedule1
+from .schedule_6months import schedule2
 
 visit_schedule1 = VisitSchedule(
     name='visit_schedule1',
@@ -11,5 +12,6 @@ visit_schedule1 = VisitSchedule(
     previous_visit_schedule=None)
 
 visit_schedule1.add_schedule(schedule1)
+visit_schedule1.add_schedule(schedule2)
 
 site_visit_schedules.register(visit_schedule1)
